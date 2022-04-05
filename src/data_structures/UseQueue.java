eaf9030a54327959a4d369c72a472e92b3367324
asharif02 <asharif02@gmail.com>
2022-04-05 13:04:59 -0400

    Implemented add, peek, remove and poll methods to data_structures > UseQueue (still need to use for-each and while-loop with iterator)

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,15 +1,56 @@
 package data_structures;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
      * Demonstrate how to use Queue that includes add, peek, remove & poll methods.
      * Use For-Each loop and While-Loop with Iterator to retrieve data.
      *
-     * Store and retrieve data from/to a database table.
+     * Store and retrieve data from/to a database table. (???)
      */
 
     public static void main(String[] args) {
+
+        /** ADD, PEEK, REMOVE AND POLL METHODS
+        Queue<String> restaurantLine = new LinkedList<>();
+
+        // ADD - adding people to our restaurant line
+        restaurantLine.add("John");
+        restaurantLine.add("Hannah");
+        restaurantLine.add("Abdi");
+        restaurantLine.add("Aaron");
+        restaurantLine.add("Jay");
+        System.out.println("Initial Queue: " + restaurantLine);
+
+        // PEEK- take a peek but don't remove person from line
+        restaurantLine.peek();
+        System.out.println("Peeking at: " + restaurantLine.peek());
+
+        // REMOVE - remove people from restaurant line
+        restaurantLine.remove("Abdi");
+        System.out.println("After removal: " + restaurantLine);
+
+        // POLL - grab the next person in line and remove them
+        restaurantLine.poll();
+        System.out.println("Final Queue after Poll: " + restaurantLine);
+        */
+
+        // FOR-EACH and WHILE-LOOP
+        Queue<String> q = new LinkedList<>();
+
+        q.add("A");
+        q.add("B");
+        q.add("C");
+
+        Iterator iterator = q.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 
 
 
