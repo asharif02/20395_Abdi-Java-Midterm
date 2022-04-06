df9c5713d92c2e3aafdba0109a283d6b689b9d12
asharif02 <asharif02@gmail.com>
2022-04-06 14:55:30 -0400

    math_problems section fully completed

--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -3,7 +3,7 @@ package math_problems;
 public class UnitTestingMath {
 
     /**
-     * Use this class to unit test all of the class within this package
+     * Use this class to unit test all the class within this package
      */
 
     public static void main(String[] args) {
