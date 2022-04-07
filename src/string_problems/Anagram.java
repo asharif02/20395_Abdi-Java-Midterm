820da4812023601784c92fe1fc1302ce358bf6f1
asharif02 <asharif02@gmail.com>
2022-04-07 02:29:38 -0400

    Finished string_problems > Anagram (optimized)

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -20,6 +20,9 @@ public class Anagram {
 
         System.out.println(isAnagram("Abdi", "Sami"));
         System.out.println(isAnagram("Cat", "Act"));
+        System.out.println(isAnagram("fArT", "RaFt"));
+        System.out.println(isAnagram("Earth Day", "Heart Day"));
+        System.out.println(isAnagram("netflix", "youtube"));
 
     }
 
@@ -32,12 +35,20 @@ public class Anagram {
     public static String isAnagram(String word1, String word2) {
 
         // this is just in case we type in aLtErNaTiNg CaPs
-        String x = word1.toLowerCase();
-        String y = word2.toLowerCase();
+        String x = word1;
+        String y = word2;
 
+        // this handles blank space and mixed-case characters
+        x = x.toLowerCase();
+        y = y.toLowerCase();
+        x = x.replace(" ", "");
+        y = y.replace(" ", "");
+
+        // store characters into array
         char[] array1 = x.toCharArray();
         char[] array2 = y.toCharArray();
 
+        // sorts characters
         Arrays.sort(array1);
         Arrays.sort(array2);
 
