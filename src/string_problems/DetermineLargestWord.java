9efdddd1bb3634507a3b488fc4f2e0fedf80ae12
asharif02 <asharif02@gmail.com>
2022-04-07 02:51:23 -0400

    Finished string_problems > DetermineLargestWord

--- src/string_problems/DetermineLargestWord.java
+++ src/string_problems/DetermineLargestWord.java
@@ -5,15 +5,18 @@ import java.util.Map;
 
 public class DetermineLargestWord {
 
+    /** INSTRUCTIONS
+     * Implement a solution to find the length of the longest word in the given sentence below
+     *
+     * Your solution should return a map containing the length of the word as the key & the word itself as the value
+     *      "10: biological"
+     */
+
     public static void main(String[] args) {
-        /*
-         Implement a solution to find the length of the longest word in the given sentence below
-         Your solution should return a map containing the length of the word as the key & the word itself as the value
-            "10: biological"
-         */
+
         String s = "Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
-
+        System.out.println(findTheLargestWord(s));
 
     }
 
@@ -21,7 +24,18 @@ public class DetermineLargestWord {
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
 
-        // Implement here
+        String[] words = wordGiven.split(" ");
+
+        int maxLength = 0;
+        for(int i = 0; i < words.length; i++){
+
+            if(words[i].length() > maxLength){
+                maxLength = words[i].length();
+                st = words[i];
+            }
+        }
+
+        map.put(maxLength, st);
 
         return map;
     }
