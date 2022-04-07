31ac622bb0b9adcf938ebc7e194bcae4b17c7885
asharif02 <asharif02@gmail.com>
2022-04-07 03:39:22 -0400

    95% complete - still need to find the average length of all the words

--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -2,13 +2,32 @@ package string_problems;
 
 public class Palindrome {
 
-    /*
-      A palindrome is a word that can be reversed, and still remain the same.
-      Example: MOM, DAD, MADAM, RACECAR
-
-      Create a method to check if any given String is a palindrome or not.
+    /**
+     * A palindrome is a word that can be reversed, and still remain the same.
+     * Example: MOM, DAD, MADAM, RACECAR
+     *
+     * Create a method to check if any given String is a palindrome or not.
      */
 
     // Implement here
+
+    public static void main(String[] args) {
+        isPalindrome("poop");
+    }
+
+    public static boolean isPalindrome (String word) {
+
+        String a = "mom";
+        String reverse = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reverse += word.charAt(i);
+            System.out.println(reverse);
+
+        }
+
+        return false;
+    }
+
 
 }
