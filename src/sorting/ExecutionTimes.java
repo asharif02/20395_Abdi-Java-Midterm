f55f7dcbeef8361f0b248748a394ed57290ff3a9
asharif02 <asharif02@gmail.com>
2022-04-07 13:33:14 -0400

    Modified sorting > ExecutionTimes

--- src/sorting/ExecutionTimes.java
+++ src/sorting/ExecutionTimes.java
@@ -2,8 +2,8 @@ package sorting;
 
 public class ExecutionTimes {
 
-    // LINK TO GOOGLE SPREADSHEET BELOW:
+    /** LINK TO GOOGLE SPREADSHEET BELOW: */
 
-    // https://docs.google.com/spreadsheets/d/1WhjV6db6wMLyOfWg0wIIZzTOarXDdHoh7TK06PU9_JE/edit?usp=sharing
+    String URL = "https://docs.google.com/spreadsheets/d/1WhjV6db6wMLyOfWg0wIIZzTOarXDdHoh7TK06PU9_JE/edit?usp=sharing";
 
 }
