d2bfa193a504378b5433e8ddb7e684330599474d
asharif02 <asharif02@gmail.com>
2022-04-09 19:56:36 -0400

    working on company_app_design...

--- src/company_app_design/CompanyEmployee.java
+++ src/company_app_design/CompanyEmployee.java
@@ -1,5 +1,7 @@
 package company_app_design;
 
+import java.util.ArrayList;
+
 public class CompanyEmployee {
 
     /** INSTRUCTIONS
@@ -11,8 +13,44 @@ public class CompanyEmployee {
      *  The goal of this application is to provide basic employee information services
      *  Try to think like a Software Developer
      */
+
     public static void main(String[] args) {
 
+        EmployeeInfo e1 = new EmployeeInfo("John", 1, "Manager", "john@gmail.com", "Marketing", 90_000);
+
+        System.out.println("Emp ID: " + e1.employeeId());
+
+        System.out.println("Emp Dept:" + e1.getDepartment());
+
+        System.out.println("Emp email: " + e1.getEmail());
+
+        System.out.println("Emp Salary: " + e1.calculateSalary());
+
+
+        e1.assignDepartment("Engineering");
+
+        System.out.println("new department: " + e1.getDepartment());
+
+        System.out.println("Num of employees: " + e1.getNumOfEmployees());
+        System.out.println();
+        e1.isFullTime(false);
+        System.out.println("Benefits for part time employees: ");
+        e1.benefits();
+        System.out.println();
+        e1.isFullTime(true);
+        System.out.println("Benefits for full-time employees: ");
+        e1.benefits();
+        System.out.println();
+
+        System.out.println("Employee Bonus: " + EmployeeInfo.calculateAnnualBonus(90_000, 4));
+        System.out.println();
+        e1.printSlogan();
+
+
+
+
+
     }
+
 
 }
