7391be393cbf15e02982fb7a9a5c9418e92c3fa4
asharif02 <asharif02@gmail.com>
2022-04-09 20:12:30 -0400

    updated DataReader

--- src/data_structures/DataReader.java
+++ src/data_structures/DataReader.java
@@ -1,97 +1,101 @@
 package data_structures;
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import databases.SharedStepsDatabase;
 
 import java.io.*;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class DataReader {
 
-    /** INSTRUCTIONS
+
+    /**
      * Create an API to read the .txt file and print it to the console.
-     *      HINT: Use BufferedReader class
+     * HINT: Use BufferedReader class
      * Use try-catch block to handle any exceptions
-     *
+     * <p>
      * Store and retrieve the file to/from a database table.
-     *
+     * <p>
      * After reading from file using BufferedReader API, store each word, first into a LinkedList. Each word
      * should construct a node in LinkedList. Second, do the same, but with a Stack.
      * Finally, traverse through the list\stack & retrieve each item in `FIFO` order and `FILO` order, respectively
-     *
+     * <p>
      * Demonstrate how to use a stack using push, peek, search & pop methods.
      * Use For-Each & While-loop with Iterator to retrieve/print data.
      **/
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
         String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car";
+        StringBuilder fileContents = new StringBuilder();
+        LinkedList<String> wordList = new LinkedList<>();
+        SharedStepsDatabase ssdb = new SharedStepsDatabase();
 
-        String line;
 
-        /** Part 1 - read file and print to console */
-
+//        Part One.
         try {
             BufferedReader reader = new BufferedReader(new FileReader(textFilePath));
-            while((line = reader.readLine()) != null)
+            String line;
+
+            while ((line = reader.readLine()) != null)
                 System.out.println(line);
             reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException exception) {
+            exception.printStackTrace();
 
-        /** Part 2 - store and retrieve from database (does not matter how you do this) */
+            System.out.println(textFilePath);
 
+//        Part Two.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /** Part 3 - store each word into LinkedList
-
-        String fileContents = "";
-        LinkedList<String> wordList = new LinkedList<>();
-        try {
             BufferedReader reader = new BufferedReader(new FileReader(textFilePath));
-            while((line = reader.readLine()) != null)
-                fileContents += line;
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            ssdb.insertString("text_file", "file_contents", fileContents.toString());
+            String query = "select * from text_file;";
+
+            List<List<String>> results = ssdb.executeQueryReadAll(query);
+
+
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+
+//        Part Three.
+
+            try {
+                reader = new BufferedReader(new FileReader(textFilePath));
+                String line;
+                while ((line = reader.readLine()) != null)
+                    fileContents.append(line);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            wordList.add((fileContents.toString()));
+            System.out.println(wordList);
+
+
+            File file = new File(textFilePath);
+            Scanner scan = null;
+            try {
+                scan = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            wordList = new LinkedList<>();
+            if (scan != null) {
+                while (scan.hasNextLine()) {
+                    fileContents = new StringBuilder(fileContents.toString().concat(scan.nextLine() + "\n"));
+                }
+            }
+            wordList.add(fileContents.toString());
+            System.out.print(wordList);
+
+
         }
-
-
-        wordList.add((fileContents));
-        System.out.println(wordList);
-         */
-
-        /** Part 3 cont... using Scanner
-
-
-//        File file = new File(textFilePath);
-//        Scanner scan = new Scanner(file);
-//        LinkedList<String> wordList = new LinkedList<>();
-//        String fileContent = "";
-//        while(scan.hasNextLine()) {
-//            fileContent = fileContent.concat(scan.nextLine() + "\n");
-//        }
-//
-//        wordList.add(fileContent);
-//        System.out.print(wordList);
-//        System.out.println(wordList.getLast());
-         */
-
     }
 }
